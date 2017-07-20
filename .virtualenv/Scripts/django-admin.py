5e74bd181fa3409fed5eb3e48ebfce829a3067cd
surlymatt <mattswan73@gmail.com>
2017-07-20 23:27:12 +1000

    Clean up

--- .virtualenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\matt\documents\projects\django\pyrally\virtualenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ src/logic_analyser.py
@@ -0,0 +1,38 @@
+﻿import nidaqmx
+import pprint
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from nidaqmx.constants import (LineGrouping)
+pp = pprint.PrettyPrinter(indent=4)
+
+numSamples = 10000
+daqDevice = "Dev3/port0/line0:7"
+
+with nidaqmx.Task() as task:
+
+    task.di_channels.add_di_chan(daqDevice,
+                                 line_grouping = LineGrouping.CHAN_PER_LINE)
+
+    t0 = time.time()
+    data = task.read(number_of_samples_per_channel=numSamples)
+    t1 = time.time()
+
+interval = t1-t0
+speed = numSamples / interval
+print(round(interval,2),"seconds for", numSamples, "reads.")
+print(round(speed,1),"samples per second.")
+
+traces = np.zeros((numSamples, 8), np.object)
+for n in range(numSamples):
+    for m in range(8):
+        traces[n][m] = m + 0.7* int(data[m][n])
+
+plt.title('Poor Mans Logic Analyser - niDAQmx')
+plt.xlabel('Sample number')
+plt.ylabel('Pin state')
+plt.plot(traces)
+plt.grid(True)
+plt.savefig("plot.png")
+plt.show()
